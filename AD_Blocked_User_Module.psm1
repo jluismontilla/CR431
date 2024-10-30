edc1de2fd5ad0a414c5b92b8ec10ec7d3e7be8ad
jluismontilla <jluismontillam@gmail.com>
2024-10-29 20:19:39 -0400

    New push with modifications from Cursor

--- AD_Blocked_User_Module.psm1
+++ AD_Blocked_User_Module.psm1
@@ -2,3 +2,29 @@ function Get-LockedOutUser {
     Get-ADUser -Filter {LockedOut -eq $true} -Properties LockedOut | 
     Select-Object Name, SamAccountName, LockedOut
 }
+
+function Send-LockoutAlert {
+    param (
+        [string]$EmailAddress,
+        [string]$UserName
+    )
+    $subject = "Alert: User Account Locked Out"
+    $body = "The user account $UserName has been locked out."
+    Send-MailMessage -To $EmailAddress -From "alert@yourdomain.com" -Subject $subject -Body $body -SmtpServer "smtp.yourdomain.com"
+}
+
+function Log-LockoutEvent {
+    param (
+        [string]$UserName
+    )
+    $logPath = "C:\Logs\AD_Lockout_Events.log"
+    $message = "$(Get-Date): User account $UserName has been locked out."
+    Add-Content -Path $logPath -Value $message
+}
+
+# Example usage
+# $lockedOutUsers = Get-LockedOutUser
+# foreach ($user in $lockedOutUsers) {
+#     Send-LockoutAlert -EmailAddress "admin@yourdomain.com" -UserName $user.SamAccountName
+#     Log-LockoutEvent -UserName $user.SamAccountName
+# }
--- Practice.ps1
+++
@@ -1,9 +0,0 @@
-$params = @{
-    DisplayName = 'Ne Pas Permettre Ping'
-    Profile = 'Domain'
-    Protocol = 'ICMPv4'
-    Direction = 'Outbound'
-    Action = 'Block'
-}
-
-New-NetFirewallRule @params
